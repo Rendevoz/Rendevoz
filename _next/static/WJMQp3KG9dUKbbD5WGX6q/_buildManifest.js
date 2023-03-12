ef1a48978391076f1a8b665e101bc6172ce91554
Attiley <yywz.hero@gmail.com>
2023-03-13 01:52:14 +0800

    Updates

---
+++ _next/static/WJMQp3KG9dUKbbD5WGX6q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/761-353b2b010738a7d2.js","static/chunks/pages/index-70bfa8c3994c1db1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
